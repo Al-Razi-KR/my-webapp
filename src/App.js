117253287036339f85cc8a62b2314402547f71fc
Razi Razi Karappamveetil Rasheed <razik@xlnx.xilinx.com>
2021-01-14 11:24:54 +0800

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
 
+const PRODUCTS = [
+  {category: 'Sporting Goods', price: '$49.99', stocked: true, name: 'Football'},
+  {category: 'Sporting Goods', price: '$9.99', stocked: true, name: 'Baseball'},
+  {category: 'Sporting Goods', price: '$29.99', stocked: false, name: 'Basketball'},
+  {category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+  {category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+  {category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
 
-function Name(props) {
+function ProductList(props) {
+  const element = props.products.map((product) => 
+    <li>{product.name}</li>
+  );
   return (
-    <div>
-        <h1>Hello, {props.name}!</h1>
-    </div>
+    <ul>{element}</ul>
   );
 }
 
 function App() {
     return (
         <div>
-            <Name name="Razi"/>
-            <Name name="Zeno"/>
-            <Name name="Pluto"/>
+           <ProductList products={PRODUCTS}/>
         </div>
     )
 }
