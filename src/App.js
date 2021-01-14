d8bc504514e7a290061eef1c4c4c72b8f3d92fd9
Razi Razi Karappamveetil Rasheed <razik@xlnx.xilinx.com>
2021-01-14 11:06:34 +0800

    edit app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import './App.css';
 
-function App() {
+
+function Name(props) {
   return (
     <div>
-      
+        <h1>Hello, {props.name}!</h1>
     </div>
   );
+}
+
+function App() {
+    return (
+        <div>
+            <Name name="Razi"/>
+            <Name name="Zeno"/>
+            <Name name="Pluto"/>
+        </div>
+    )
 }
 
 export default App;
