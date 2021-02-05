e99491d1a2de38c7b79d3a30fdea32a6bd321d53
Al-Razi <mohammedrazikr9419@gmail.com>
2021-02-05 12:58:50 +0800

    updates

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import React from 'react';
+import { hot } from 'react-hot-loader';
 import './App.css';
 
 function App() {
     return (
         <div>
-
+          
         </div>
     )
 }
 
-export default App;
+export default hot(module)(App);
